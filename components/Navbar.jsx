295ed89a3bfbd554200d845f246f9ac6d346c70f
Patrick Alyson <patrickalyson@protonmail.com>
2023-03-07 14:17:06 -0300

    Finished navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,35 @@
-'use client';
+"use client";
 
-const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
-);
+import { motion } from "framer-motion";
+import { navVariants } from "../utils/motion";
+
+function Navbar() {
+  return (
+    <motion.nav
+      variants={navVariants}
+      initial="hidden"
+      animate="show"
+      className="xPaddings py-8 relative"
+    >
+      <div className="absolute w-[50%] inset-0 gradient-01"></div>
+
+      <div className="innerWidth mx-auto flex justify-between gap-8">
+        <img
+          src="/search.svg"
+          alt="search"
+          className="w-[24px] h-[24px] object-contain"
+        />
+        <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+          METAVERSUS
+        </h2>
+        <img
+          src="/menu.svg"
+          alt="menu"
+          className="w-[24px] h-[24px] object-contain"
+        />
+      </div>
+    </motion.nav>
+  );
+}
 
 export default Navbar;
