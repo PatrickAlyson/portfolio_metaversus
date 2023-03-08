9b6fa857e6f69040ed0cdd6e2db250390fa686ff
Patrick Alyson <patrickalyson@protonmail.com>
2023-03-08 14:39:25 -0300

    Finished WhatsNew section

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,6 +1,11 @@
-const StartSteps = () => (
-  <div>
-    start steps
+const StartSteps = ({ number, text }) => (
+  <div className="flexCenter flex-row">
+    <div className="flexCenter h-[70px] w-[70px] rounded-[24px] bg-[#323f5d]">
+      <p className="text-[20px] font-bold text-white">0{number}</p>
+    </div>
+    <p className="ml-[30px] flex-1 text-[18px] font-normal leading-[32px] text-[#B0B0B0]">
+      {text}
+    </p>
   </div>
 );
 
--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,8 +1,42 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText, StartSteps, TitleText } from "../components";
+import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
+import { startingFeatures } from "../constants";
 
 const GetStarted = () => (
-  <section>
-    Get Started Section
+  <section className="paddings relative z-10">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className="innerWidth mx-auto flex flex-col gap-8 lg:flex-row"
+    >
+      <motion.div
+        variants={planetVariants("left")}
+        className="flexCenter flex-1"
+      >
+        <img
+          src="/get-started.png"
+          alt="get-started"
+          className="w-[90%]h-[90%] object-contain"
+        />
+      </motion.div>
+      <motion.div
+        variants={fadeIn("left", "tween", 0.2, 0.1)}
+        className="flex flex-[0.75] flex-col justify-center"
+      >
+        <TypingText title="| How Metaversus Works" />
+        <TitleText title={<>Get started with just a few clicks</>} />
+        <div className="mt-[31px] flex max-w-[370px] flex-col gap-[24px]">
+          {startingFeatures.map((feature, index) => (
+            <StartSteps key={feature} number={index + 1} text={feature} />
+          ))}
+        </div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
