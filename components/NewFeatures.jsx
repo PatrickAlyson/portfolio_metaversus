98f2d9f9ed74c3099e098dba43e7c8bcfddca3b4
Patrick Alyson <patrickalyson@protonmail.com>
2023-03-08 15:17:57 -0300

    made some changes on the Whatsnew section

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,6 +1,14 @@
-const NewFeatures = () => (
-  <div>
-    New Features
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  <div className="flex min-w-[210px] flex-1 flex-col sm:max-w-[250px]">
+    <div className="flexCenter h-[70px] w-[70px] rounded-[24px] bg-[#323f5d]">
+      <img src={imgUrl} alt="icon" className="h-1/2 w-1/2 object-contain" />
+    </div>
+    <h1 className="mt-[26px] text-[24px] font-bold leading-[30px] text-white">
+      Title {title}
+    </h1>
+    <p className="mt-[16px] flex-1 text-[18px] font-normal leading-[32px] text-[#b0b0b0]">
+      {subtitle}
+    </p>
   </div>
 );
 
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,8 +1,42 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText, NewFeatures, TitleText } from "../components";
+import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
+import { newFeatures } from "../constants";
 
 const WhatsNew = () => (
-  <section>
-    What's new section
+  <section className="paddings relative z-10">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className="innerWidth mx-auto flex flex-col gap-8 lg:flex-row"
+    >
+      <motion.div
+        variants={fadeIn("right", "tween", 0.2, 0.1)}
+        className="flex flex-[0.75] flex-col justify-center"
+      >
+        <TypingText title="| What's New?" />
+        <TitleText title={<>What's new about Metaversus?</>} />
+        <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
+          {newFeatures.map((feature) => (
+            <NewFeatures key={feature.title} {...feature} />
+          ))}
+        </div>
+      </motion.div>
+      <motion.div
+        variants={planetVariants("right")}
+        className="flexCenter flex-1"
+      >
+        <img
+          src="/whats-new.png"
+          alt="get-started"
+          className="w-[90%]h-[90%] object-contain"
+        />
+      </motion.div>
+    </motion.div>
   </section>
 );
 
