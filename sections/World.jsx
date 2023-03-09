742fbb00707677f14e5e0faf5febc175357a4ded
Patrick Alyson <patrickalyson@protonmail.com>
2023-03-09 07:54:59 -0300

    Finished World section

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,48 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText, TitleText } from "../components";
+import { staggerContainer, fadeIn } from "../utils/motion";
 
 const World = () => (
-  <section>
-    World section
+  <section className="paddings relative z-10">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className="innerWidth mx-auto flex flex-col"
+    >
+      <TypingText title="| People on the world" textStyles="text-center" />
+      <TitleText
+        title={
+          <>
+            Track friends around you and invite them to play together in the
+            same world
+          </>
+        }
+        textStyles="text-center"
+      />
+
+      <motion.div
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        className="relative mt-[68px] flex h-[550px] w-full"
+      >
+        <img src="/map.png" alt="map" className="h-full w-full object-cover" />
+
+        <div className="absolute bottom-20 right-20 h-[70px] w-[70px] rounded-full bg-[#5d6680] p-[6px]">
+          <img src="people-01.png" alt="people" className="h-full w-full" />
+        </div>
+
+        <div className="absolute top-10 left-20 h-[70px] w-[70px] rounded-full bg-[#5d6680] p-[6px]">
+          <img src="people-02.png" alt="people" className="h-full w-full" />
+        </div>
+
+        <div className="absolute top-1/2 left-[45%] h-[70px] w-[70px] rounded-full bg-[#5d6680] p-[6px]">
+          <img src="people-03.png" alt="people" className="h-full w-full" />
+        </div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
