26727a6a2202f0ee243edf7dba1b7a1e5e77c3df
Patrick Alyson <patrickalyson@protonmail.com>
2023-03-09 16:45:38 -0300

    Small changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client";
-import { useState, useEffect } from "react";
 
+import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { navVariants, modalVariants } from "../utils/motion";
 import { socials } from "../constants";
